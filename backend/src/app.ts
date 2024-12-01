60f45669eabb18817dac2d055e9501b91aed4906
Adityo Gizwanda <adigiz@yahoo.com>
2024-12-01 12:28:55 +0700

    change middleware position

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -15,13 +15,11 @@ AppDataSource.initialize()
   })
   .catch((error) => console.log(error));
 
+app.use("/api/patients", patientRoutes);
+app.use("/api/auth", userRoutes);
 app.use((req, res, next) => {
   res.status(404).send({ message: "Not found" });
 });
-
-app.use("/api/patients", patientRoutes);
-app.use("/api/auth", userRoutes);
-
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
